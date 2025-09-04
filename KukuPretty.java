7a935d97e5f3098c7f706c4bf61afb083c8e21e9
SAOpvq <saori222pvq@gmail.com>
2025-09-04 23:26:49 +0900

    Add formatted multiplication table (KukuPretty)

---
+++ KukuPretty.java
@@ -0,0 +1,20 @@
+public class KukuPretty {
+    public static void main(String[] args) {
+
+        System.err.print("    |");
+        for (int j = 1; j <= 9; j++) {
+            System.out.printf("%3d", j);
+        }
+        System.out.println();
+        System.out.println("----+---------------------------");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%3d |", i);
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
